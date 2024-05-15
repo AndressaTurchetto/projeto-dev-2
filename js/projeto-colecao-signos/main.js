0a4813df73e9ed79f3c93bf7fa3ebf1193f041dc
andressa turchetto <and.turchetto@gmail.com>
2024-05-14 23:33:10 -0300

    Atualizando lista de exercício

---
+++ js/projeto-colecao-signos/main.js
@@ -0,0 +1,14 @@
+let colecao_signos = [
+    {"Nome": "Aquário", "dataInicio": "", "dataFim": ""},
+    {"Nome": "", "dataInicio": "", "dataFim": ""},
+    {"Nome": "", "dataInicio": "", "dataFim": ""},
+    {"Nome": "", "dataInicio": "", "dataFim": ""},
+    {"Nome": "", "dataInicio": "", "dataFim": ""},
+    {"Nome": "", "dataInicio": "", "dataFim": ""},
+    {"Nome": "", "dataInicio": "", "dataFim": ""},
+    {"Nome": "", "dataInicio": "", "dataFim": ""},
+    {"Nome": "", "dataInicio": "", "dataFim": ""},
+    {"Nome": "", "dataInicio": "", "dataFim": ""},
+    {"Nome": "", "dataInicio": "", "dataFim": ""},
+    {"Nome": "", "dataInicio": "", "dataFim": ""},
+]
